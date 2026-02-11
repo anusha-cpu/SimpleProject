bedc23b9345830fffe0c2bf8fc599bf550d98497
Your Name <you@example.com>
2026-02-11 15:01:56 +0530

    My Project

---
+++ react-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+import section from "./components/section"
+function app() {
+  return (
+    <>
+      <Header/>
+      <section />
+      </>
+  )
+}
+export default app
